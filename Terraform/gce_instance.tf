8526b451832c3bed9da6bdc39429c4d0d88e5465
unah254 <greisunah@gmail.com>
2021-05-04 23:53:19 +0300

    update-instance

--- Terraform/gce_instance.tf
+++ Terraform/gce_instance.tf
@@ -11,3 +11,28 @@ resource "google_compute_instance_from_template" "tpl" {
   project = var.gcp["project"]
   
 }
+//updating the instances
+resource "google_compute_instance_from_template" "tpl2" {
+  depends_on = [
+    google_compute_instance_template.instance_template
+  ]
+  name = "instance-from-template2"
+  zone = var.gcp["zone"]
+
+  source_instance_template = google_compute_instance_template.instance_template.id
+
+  project = var.gcp["project"]
+  
+}
+resource "google_compute_instance_from_template" "tpl3" {
+  depends_on = [
+    google_compute_instance_template.instance_template
+  ]
+  name = "instance-from-template3"
+  zone = var.gcp["zone"]
+
+  source_instance_template = google_compute_instance_template.instance_template.id
+
+  project = var.gcp["project"]
+  
+}
